4e68e77d4335fefd411b79f18dd77d668d8124ea
Võ Hoài Danh <danhvohoai2@gmail.com>
2024-12-27 08:14:16 +0700

    Update vision_transformer.py

--- vision_transformer.py
+++ vision_transformer.py
@@ -252,7 +252,7 @@ def vit_small_from_checkpoint(patch_size=16, mode = 'student', **kwargs):
         patch_size=patch_size, embed_dim=384, depth=12, num_heads=6, mlp_ratio=4,
         qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     
-    state_dict = torch.load(r"D:\K32\do_an_tot_nghiep\ADOF\weights\dino_deitsmall16_pretrain_full_checkpoint.pth")[mode]
+    state_dict = torch.load(r"dino_deitsmall16_pretrain_full_checkpoint.pth")[mode]
     state_dict = utils.remove_module_prefix(state_dict)
     
     return model, state_dict
